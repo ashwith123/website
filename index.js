53664bb7c282c1b291ddb079a9a126ba29465e7e
ashwith <ashwith9492@gmail.com>
2024-04-03 21:48:33 +0530

    array of students made step 8

--- index.js
+++ index.js
@@ -6,6 +6,18 @@ let port=8080;
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"ejs"));
+app.set(express.static(path.join(__dirname,"public")));
+
+let post=[{usernme:"ashwith",
+        content:"i am ashwith"  
+        },
+        {usernme:"saitama",
+        content :"i am hero for fun"  
+        },
+        {usernme:"anya",
+        content:"i am anay"  
+        },
+];
 
 
 app.listen(port,()=>{
