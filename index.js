cd2294cfbf0b02a8ea93118523bba27b113944d9
ashwith <ashwith9492@gmail.com>
2024-04-03 21:35:27 +0530

    ejs express made and required

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const express=require("express")
+
+const app=express();
+
+let port=8080;
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"ejs"));
+
+
+app.listen(port,()=>{
+    console.log("app is listening");
+})
