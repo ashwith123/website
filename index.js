f1b1d9d2a2fc92d3ed5610a14273e9d2526d6929
ashwith <ashwith9492@gmail.com>
2024-04-05 12:06:33 +0530

    had to add a code snippet from gpt as req,bidy was not being printed and had to change arr a=name to posts and give names to each textbox

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const express=require("express")
+const express=require("express");
+const bodyParser = require('body-parser');
+
 const path = require("path"); 
 
 const app=express();
 
 let port=8080;
-
+/*all path are set using below three lines   */
 app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("views", path.join(__dirname,"views"));
 app.use(express.static(path.join(__dirname,"public")));
 
-let post=[{username:"ashwith",
+let posts=[{username:"ashwith",
         content:"i am ashwith"  
         },
         {username:"saitama",
@@ -24,9 +27,16 @@ app.get("/posts",(req,res)=>{
     res.render("index.ejs",{post});
 });
 
+/* rendering in new post */
 app.get("/posts/new",(req,res)=>{
     res.render("new.ejs");
 });
+
+app.post("/posts",(req,res)=>{
+    console.log(req.body);
+    res.send("post sucessfully made");
+})
+
 
 app.get("/",(req,res)=>{
     res.send("server is working");
